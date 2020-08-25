6af7f618ce27a500ff97ba2c78d7c5308b6529cd
iamdiyn <68436821+iamdiyn@users.noreply.github.com>
2020-08-25 21:06:53 +0530

    Add files via upload
    Program to find a  given year is a leap year or not.

---
+++ oddandeven.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main()
+
+{
+	int j;
+	int n=1;
+	int add=0;
+	int mul=1;
+	
+	vector<int>array={2,1,2,3,5,6,};
+
+	for(int i=0;i<array.size();i++)
+	{
+		j=array[i];
+		if(j==2*n)
+		{
+			add+=array[i];
+
+		}
+		else
+		{
+			mul=array[i]*mul;
+
+		}
+
+	}
+	cout<<"sum:"<<add<<"\n";
+	cout<<"multi:"<<mul<<"\n";
+	
+
+
+}
